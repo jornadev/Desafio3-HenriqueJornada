bd744ea4b5909b5e50db8262caafdeac99ffae34
Henrique Jornada <jornadahenrique12@gmail.com>
2025-03-05 17:30:25 -0300

    [fix] | created structure ms-ticket-manager

---
+++ ms-ticket-manager/src/main/java/com/compass/MsTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTicketManagerApplication.class, args);
+	}
+
+}
