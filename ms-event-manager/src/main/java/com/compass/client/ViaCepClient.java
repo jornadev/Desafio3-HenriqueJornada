41f7f1fa3eb445d847dfa1886ac2b9a83cf7a681
Henrique Jornada <jornadahenrique12@gmail.com>
2025-03-08 12:47:31 -0300

    [feat] | created ViaCep interface

---
+++ ms-event-manager/src/main/java/com/compass/client/ViaCepClient.java
@@ -0,0 +1,12 @@
+package com.compass.client;
+
+import com.compass.model.Address;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepClient {
+    @GetMapping("/{cep}/json")
+    Address getAddress(@PathVariable String cep);
+}
